cad7dedec3c0b4b4137962db87232d277977a676
bowdenshot <anthonyelavezzo@gmail.com>
2021-11-15 18:28:25 -0800

    add toilet marker to map

--- client/public/env.js
+++ client/public/env.js
@@ -1,1 +1,4 @@
-window.env = {};
+window.env = {
+  "GOOGLE_MAPS_API_KEY": "AIzaSyAiuFj_pw5GEzLSPjF2QB4FPRaAO_2oJLg",
+  "GOOGLE_MAPS_ID": "abiding-rhythm-332220"
+};
--- client/src/template/Map.js
+++ client/src/template/Map.js
@@ -5,7 +5,8 @@ import {
     GoogleMap,
     useLoadScript,
     Marker,
-    InfoWindow
+    InfoWindow,
+    MarkerClusterer
 } from '@react-google-maps/api'
 import { formatRelative } from 'date-fns'
 
@@ -14,8 +15,8 @@ import '@reach/combobox/styles.css'
 
 const libraries = ['places']
 const mapContainerStyle = {
-    width: "100vw",
-    height: '100vh',
+    width: "800px",
+    height: "800px",
 };
 
 const options = {
@@ -31,21 +32,42 @@ const center = {
 
 const Map = () => {
 
-
+    const onMapClick = React.useCallback((event) => {
+        setMarkers(current => [ ...current, {
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng(),
+            time: new Date
+        }]);
+    }, [])
     const { isLoaded, loadError} = useLoadScript({
         id: process.env.GOOGLE_MAPS_ID || env.GOOGLE_MAPS_ID,
         googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY || env.GOOGLE_MAPS_API_KEY,
         libraries
     });
 
-   
+    const [markers, setMarkers] = React.useState([]);
 
     if (loadError) return 'Error lading maps';
     if (!isLoaded) return "Loading Maps";
 
+   const image = {
+        url: "/toilet.svg",
+        scaledSize: new window.google.maps.Size(20,32),
+        origin: new window.google.maps.Point(0,0),
+        anchor: new window.google.maps.Point(10,32)
+   };
+
     return (<div>
-        <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center} options={options}></GoogleMap>
-    </div>
+        <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center} options={options} onClick={onMapClick}>
+
+            {markers.map(marker => <Marker key={marker.time.toISOString()}
+            position={{lat: marker.lat, lng: marker.lng}}
+            icon={image}
+            />)}
+
+
+        </GoogleMap>
+    </div> 
     )
 }
 
