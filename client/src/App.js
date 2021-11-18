a28927248ca58372f82e142488428034419333e4
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-11-18 00:34:06 -0800

    added accidentally deleted sections

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,43 @@ import Sample from "./template/Sample";
 import Footer from "./template/Footer";
 import Signup from "./template/Signup";
 import Login from "./template/Login";
+import ToiletForm from "./template/Form";
+import Map from './template/Map'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import Home from "./template/Home"
 
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    console.log('graphQLErrors', graphQLErrors);
+  }
+  if (networkError) {
+    console.log('networkError', networkError)
+  }
+})
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const link = ApolloLink.from([errorLink, httpLink])
+
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(link),
   cache: new InMemoryCache(),
 });
 
@@ -33,16 +59,27 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <Router>
-      <Navbar />
-      <Landing />
-      <Switch>
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-      <Sample />
-      <Footer />
-    </Router>
+      <Router>
+        <Navbar />
+        <Landing />
+        <Map />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/add">
+            <ToiletForm />
+          </Route>
+          <Route path="*" component={Home}/>
+        </Switch>
+        <Sample />
+        <Footer />
+      </Router>
     </ApolloProvider>
   );
 }
