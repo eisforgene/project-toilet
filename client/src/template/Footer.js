66b37ea56b30e2faac3fba54e20ea792cba8a90b
eisforgene <82473195+eisforgene@users.noreply.github.com>
2021-11-18 11:00:45 -0800

    adjusted footer

--- client/src/template/Footer.js
+++ client/src/template/Footer.js
@@ -2,14 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="text-center text-lg-start bg-light text-mute">
-      <section class="d-flex justify-content-center p-4 border-bottom">
-        <div>
-          <p className="text-muted small mb-4 mb-lg-0">
-            &copy; Toilet SOS 2021. All Rights Reserved.
-          </p>
+    <footer className="footer bg-light">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 h-100 text-center text-lg-start my-auto">
+            <p className="text-muted small mb-4 mb-lg-0">
+              &copy; Toilet SOS 2021 All Rights Reserved.
+            </p>
+          </div>
         </div>
-      </section>
+      </div>
     </footer>
   );
 };
